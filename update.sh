569116981236893110713602a0774fcd0a5c1e22
Bogdan Stancescu <gutza@moongate.ro>
2018-01-02 16:43:12 +0200

    Reverted to more compatible command line for autoremove; now also removing orphaned packages

--- update.sh
+++ update.sh
@@ -25,8 +25,26 @@ sudo apt-get dist-upgrade -y || {
 echo ""
 echo "Removing unnecessary packages"
 echo "============================="
-sudo apt autoremove -y || {
+sudo apt-get autoremove --purge -y || {
 	echoerr "Failed removing unnecessary packages; aborting."
+	exit 1
+}
+
+command -v deborphan > /dev/null || {
+	echo ""
+	echo "Installing deborphan"
+	echo "===================="
+	sudo apt-get install deborphan -y || {
+		echoerr "Failed installing debophpan; aborting."
+		exit 1
+	}
+}
+
+echo ""
+echo "Removing orphaned packages"
+echo "=========================="
+sudo apt-get remove `deborphan` --purge || {
+	echoerr "Failed removing orphaned packages; aborting."
 	exit 1
 }
 
