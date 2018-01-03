822f48eea0f93b54afd0ecbbf996a4ad13dfba90
Bogdan Stancescu <bogdan@moongate.ro>
2018-01-03 22:55:14 +0200

    Properly checking if you're running Raspbian before attempting to verify if you're running the latest distro version

--- update.sh
+++ update.sh
@@ -48,29 +48,40 @@ sudo apt-get remove `deborphan` --purge -y || {
 	exit 1
 }
 
-command -v curl > /dev/null &&
 command -v lsb_release > /dev/null &&
 {
-	echo ""
-	echo "Checking if you're running the latest distribution"
-	echo "=================================================="
-	LATEST_ZIP=$(curl --silent --location --head --output /dev/null --write-out '%{url_effective}' -- https://downloads.raspberrypi.org/raspbian_latest) && {
-		CURRENT_DISTRO=$(lsb_release -cs) && {
-			echo $LATEST_ZIP | grep -q $CURRENT_DISTRO && {
-				echo "You're running $CURRENT_DISTRO, which is the latest release."
+	DISTRIBUTOR_ID=$(lsb_release -is)
+	if [ $DISTRIBUTOR_ID = "Raspbian" ]
+	then
+		command -v curl > /dev/null &&
+		{
+			echo ""
+			echo "Checking if you're running the latest Raspbian distribution"
+			echo "==========================================================="
+			LATEST_ZIP=$(curl --silent --location --head --output /dev/null --write-out '%{url_effective}' -- https://downloads.raspberrypi.org/raspbian_latest) && {
+				CURRENT_DISTRO=$(lsb_release -cs) && {
+					echo $LATEST_ZIP | grep -q $CURRENT_DISTRO && {
+						echo "You're running $CURRENT_DISTRO, which is the latest Raspbian release."
+					} || {
+						echo "You're running an old distribution. You're running $CURRENT_DISTRO, but the latest image is $LATEST_ZIP"
+						echo "You can edit /etc/apt/sources.list and run the updater again -- but that's dangerous, make sure you back up your SD card first!"
+					}
+				} || {
+					echo "Failed executing lsb_release; skipping distro image check."
+				}
 			} || {
-				echo "You're running an old distribution. You're running $CURRENT_DISTRO, but the latest image is $LATEST_ZIP"
-				echo "You can edit /etc/apt/sources.list and run the updater again -- but that's dangerous, make sure you back up your SD card first!"
+				echo "Failed retrieving the latest distro image; skipping distro image check."
 			}
 		} || {
-			echo "Failed executing lsb_release; skipping distro image check."
+			echo "curl isn't installed; skipping distro image check. If you want to enable it, run"
+			echo "sudo apt-get install curl"
+			echo "and then run the updater again."
 		}
-	} || {
-		echo "Failed retrieving the latest distro image; skipping distro image check."
-	}
+	fi
 } || {
-	echo "curl and/or lsb_release aren't installed; skipping distro image check. If you want to enable it, run"
-	echo "sudo apt-get install curl lsb_release"
+	echo "lsb_release not installed; can't tell which distribution you're running."
+	echo "If you're running Raspbian, you might want to install lsb_release in order to make sure you're running the latest distro version:"
+	echo "sudo apt-get install lsb_release"
 	echo "and then run the updater again."
 }
 
